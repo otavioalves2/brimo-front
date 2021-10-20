4f6b4aa08f4925af5e143197a8452d2d8e0283e2
Otávio Sales <otavio.sales@meuvarejo360.com.br>
2021-10-20 20:28:40 -0300

    feat: formulario inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormComponentComponent } from './form-component/form-component.component';
+import {InputMaskModule} from 'primeng/inputmask';
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    InputMaskModule,
+    InputTextModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
